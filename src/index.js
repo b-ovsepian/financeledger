a45cd41423aef5d360684783db0dff8d95e31749
Bagdasar Ovsepyan <bagdasar@ovsepyan.com.ua>
2021-03-15 16:38:12 +0200

    adds smooth scroll

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
 import './sass/main.scss';
-import { headerHandler } from './js/headerHandler';
+import $ from 'jquery';
 import lightbox from 'lightbox2/dist/js/lightbox';
 
+window.onscroll = function () {
+  headerHandler();
+};
+
+const header = document.getElementById('header');
+
+function headerHandler() {
+  if (window.pageYOffset > header.offsetHeight) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
+  }
+}
+
+function scrollHandler() {
+  $('.site-nav__link').on('click', function (event) {
+    event.preventDefault();
+    const target = $(this);
+    const element = target.attr('href');
+    $('body, html').animate(
+      {
+        scrollTop: $(element).offset().top - header.offsetHeight,
+      },
+      800,
+    );
+  });
+}
+
+scrollHandler();
 window.lightbox = lightbox;
 window.lightbox.option({
   disableScrolling: true,
