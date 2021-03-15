2737de7bb8367881ef454d1099ee1a0ceb20e9b1
Bagdasar Ovsepyan <bagdasar@ovsepyan.com.ua>
2021-03-15 16:07:01 +0200

    adds cases

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import './sass/main.scss';
 import { headerHandler } from './js/headerHandler';
+import lightbox from 'lightbox2/dist/js/lightbox';
+
+window.lightbox = lightbox;
+window.lightbox.option({
+  disableScrolling: true,
+  positionFromTop: 200,
+});
